0299f7780cb5588661014bea03f5742bf1945f62
Juan Pablo <c137dts@gmail.com>
2021-01-11 13:38:25 -0500

    Inicializando el reto

---
+++ retosC/duplicateEncoder.c
@@ -0,0 +1,23 @@
+/* The goal of this exercise is to convert 
+a string to a new string where each character in the new string is "(" if that character 
+appears only once in the original string, or ")" if that character appears more than once in the 
+original string. Ignore capitalization when determining if a character is a duplicate.
+*/
+
+//url: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/c
+
+#include <string.h>
+#include <stdio.h>
+
+char duplicateEncoder(char word){
+    
+
+    return "";
+}
+
+int main(){
+    char string1 = "S";
+    char string2 = "s";
+
+    return 0;
+}
